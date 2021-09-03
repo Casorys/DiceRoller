a9e1fdaecc97bfccd030abd91b48acbc4de2060c
Tiago de Freitas | Casorys <tiagofreitas023023@gmail.com>
2021-09-03 13:35:33 -0300

    Guess the Number
    Now we can type a number to check if we guessed right or wrong. Title Background changes according to the results.

--- app/src/main/java/com/br/freitastiago/diceroller/MainActivity.kt
+++ app/src/main/java/com/br/freitastiago/diceroller/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.br.freitastiago.diceroller
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.graphics.toColor
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +18,23 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.setOnClickListener {
             /** A linha abaixo importa o TextView de um dos nossos layouts **/
+            val titleTextView: TextView = findViewById(R.id.tv_guess)
+            val luckyNumberTI: TextInputEditText = findViewById(R.id.ti_luckynumber)
+            val luckyNumber: String = luckyNumberTI.text.toString()
             val resultTextView: TextView = findViewById(R.id.textView)
             val dice = Dice(6)
             val diceRoll = dice.roll()
             /** A linha abaixo altera propriedades da TextView que importamos para a
              * variavel resultTextView **/
             resultTextView.text = diceRoll.toString()
-
+            if (diceRoll == luckyNumber.toInt()) {
+                Toast.makeText(this, "You got the right number!", Toast.LENGTH_SHORT).show()
+                titleTextView.setBackgroundColor(0xFF2fa851.toInt())
+            }
+            else {
+                Toast.makeText(this, "You missed your bet.", Toast.LENGTH_SHORT).show()
+                titleTextView.setBackgroundColor(0xFFeb3d3d.toInt())
+            }
         }
 
     }
