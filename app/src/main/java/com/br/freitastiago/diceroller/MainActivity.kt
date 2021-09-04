781054c31cd46cb8704cb7fd024893de4dc9f5cd
Tiago de Freitas | Casorys <tiagofreitas023023@gmail.com>
2021-09-04 12:50:09 -0300

    Update v1.1.0
    Updated the UI so the Dice Image is the roll button and added a score system of how many times you guessed right in a row and how many you missed. Status bar color was changed to black.

--- app/src/main/java/com/br/freitastiago/diceroller/MainActivity.kt
+++ app/src/main/java/com/br/freitastiago/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.br.freitastiago.diceroller
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.SoundEffectConstants
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,20 +13,25 @@ import androidx.core.graphics.toColor
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    
+    var scoreNumber = 0
+    var failsNumber = 0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /** A linha abaixo importa o Button de um dos nossos layouts **/
-        val rollButton: Button = findViewById(R.id.button)
+        val resultImageView: ImageView = findViewById(R.id.iv_dice)
 
 
-        rollButton.setOnClickListener {
+        resultImageView.setOnClickListener {
             /** A linha abaixo importa o TextView de um dos nossos layouts **/
+            var score: TextView = findViewById(R.id.tv_score)
+            val fails: TextView = findViewById(R.id.tv_fail)
             val nullCheck: TextInputEditText = findViewById(R.id.ti_nullCheck)
             val titleTextView: TextView = findViewById(R.id.tv_guess)
             val luckyNumberTI: TextInputEditText = findViewById(R.id.ti_luckynumber)
             val luckyNumber: String = luckyNumberTI.text.toString()
-            val resultImageView: ImageView = findViewById(R.id.iv_dice)
             val dice = Dice(6)
 
             if (luckyNumber != nullCheck.text.toString()) {
@@ -41,20 +48,27 @@ class MainActivity : AppCompatActivity() {
                 }
                 if (diceRoll == luckyNumber.toInt()) {
                     titleTextView.setBackgroundColor(0xFF2fa851.toInt())
-                }
-                else {
-                    titleTextView.setBackgroundColor(0xFFeb3d3d.toInt())
-                }
+                    scoreNumber++
+                    score.text = scoreNumber.toString()
+                    failsNumber = 0
+                    fails.text = failsNumber.toString()
+                } else {
+                        titleTextView.setBackgroundColor(0xFFeb3d3d.toInt())
+                        failsNumber++
+                        fails.text = failsNumber.toString()
+                        scoreNumber = 0
+                    score.text = scoreNumber.toString()
+                    }
             } else {
                 Toast.makeText(this, "Insert a number before rolling!", Toast.LENGTH_SHORT).show()
             }
 
-            
         }
 
     }
 
 }
+
 /** A linha abaixo Randomiza um numero **/
 class Dice(private val numSides: Int) {
     fun roll(): Int { return (1..numSides).random() }
